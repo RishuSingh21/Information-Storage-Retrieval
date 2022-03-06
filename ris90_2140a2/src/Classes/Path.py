5c990bca24e22eeed2d27edb4c029ce7f3dc05c4
Rishu Singh <95005960+RishuSingh21@users.noreply.github.com>
2022-03-06 13:54:37 -0800

    Delete ris90_2140a2 directory

--- ris90_2140a2/src/Classes/Path.py
+++
@@ -1,9 +0,0 @@
-
-# uncompleted address of preprocessed corpus.
-ResultHM1="data/result."
-
-# address of generated Text index file.
-IndexTextDir="data/indextext/"
-
-# address of generated Web index file.
-IndexWebDir="data/indexweb/"
